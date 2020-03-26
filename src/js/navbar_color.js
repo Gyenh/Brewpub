36871b8977db5977782256a636d8da71a087fc42
Gyenh <blazeog@hotmail.com>
2020-03-26 18:39:32 +0100

    jquery, cleaning files, removing svg code

---
+++ src/js/navbar_color.js
@@ -0,0 +1,45 @@
+
+var scrollpos = window.scrollY;
+var header = document.getElementById("header");
+var navcontent = document.getElementById("nav-content");
+var navaction = document.getElementById("navAction");
+var brandname = document.getElementById("brandname");
+var toToggle = document.querySelectorAll(".toggleColour");
+
+document.addEventListener('scroll', function () {
+
+    /*Apply classes for slide in bar*/
+    scrollpos = window.scrollY;
+
+    if (scrollpos > 10) {
+        header.classList.add("bg-white");
+        navaction.classList.remove("bg-white");
+        navaction.classList.add("gradient");
+        navaction.classList.remove("text-gray-800");
+        navaction.classList.add("text-white");
+        //Use to switch toggleColour colours
+        for (var i = 0; i < toToggle.length; i++) {
+            toToggle[i].classList.add("text-yellow-700");
+            toToggle[i].classList.remove("text-white");
+        }
+        header.classList.add("shadow");
+        navcontent.classList.add("bg-white");
+    }
+    else {
+        header.classList.remove("bg-white");
+        navaction.classList.remove("gradient");
+        navaction.classList.add("bg-white");
+        navaction.classList.remove("text-white");
+        navaction.classList.add("text-gray-800");
+        //Use to switch toggleColour colours
+        for (var i = 0; i < toToggle.length; i++) {
+            toToggle[i].classList.add("text-white");
+            toToggle[i].classList.remove("text-yellow-700");
+        }
+
+        header.classList.remove("shadow");
+        navcontent.classList.remove("bg-white");
+
+    }
+
+});
